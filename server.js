141b8146f992dbb4c9e77e90b24636fb3ebf84e7
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2021-01-17 20:28:18 -0800

    added the get routes and post route, re-wrote the fs.writeFile, coreccting the db.jason file being over written.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const { db } = require("./db/db.json");
-const fs = require('fs')
-const express = require('express');
-const path = require('path');
+const uniqid = require("uniqid");
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // parse incoming string or array data
@@ -11,48 +11,38 @@ app.use(express.json());
 // tells express where the static assests are
 app.use(express.static("public"));
 
-
-
-
-app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/notes.html'));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/notes.html"));
 });
 
-app.get("/api/notes", function(req, res) {
-   console.log("get request") 
-   fs.readFile("./db/db.json","utf8", (err, data) => {
-    if (err)  throw err
-    res.send(JSON.parse(data))
-
-   });
-
-
+app.get("/api/notes", function (req, res) {
+  console.log("get request");
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+app.post("/api/notes", function (req, res) {
+  console.log(req.body);
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    req.body.id = uniqid();
+    notes.push(req.body);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      console.log("data appended");
+      res.json(req.body);
+    });
+  });
 });
 
-app.post("/api/notes", function(req, res) {
-    console.log(req.body)
-    fs.writeFile('./db/db.json', JSON.stringify(req.body), (err) => {
-        if (err) throw err;
-        console.log('data appended')
-    })
-
-    
-
-
-})
-
-
-
-
-
-
-
-
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+  console.log(`API server now on port 3001!`);
+});
