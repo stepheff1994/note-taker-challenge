5c09177555bdbc9ed596cc6dc47107b31dce4cef
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2021-04-16 15:40:35 -0700

    changed from public to docs

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post("/api/notes", function (req, res) {
 });
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname + "/docs/index.html"));
 });
 
 app.listen(3001, () => {
